20ca52936585fd2a42f516aab25a825191feb84b
Brenda Luine <brenda.luine@uts.edu.au>
2023-03-07 14:53:20 +1100

    added try

--- test_firefox.py
+++ test_firefox.py
@@ -32,70 +32,72 @@ service = Service(log_path=path.devnull)
 driver = webdriver.Firefox(service=service, options=firefoxOptions)
 driver.get('https://www.giving.uts.edu.au/donate')
 
-#fill out the form
-choose_amount = driver.find_element(By.ID, 'amount225').click()
+try:
+  #fill out the form
+  choose_amount = driver.find_element(By.ID, 'amount225').click()
 
-#designation_dropdown = driver.find_element(By.ID, 's2id_PC16725_ddlDesignation').click
-#designation_dropdown.send_keys('High Impact Research')
-#designation_dropdown.select_by_visible_text('High Impact Research')
-#designation_choice = driver.find_element(By.XPATH, '//*[@id="PC16725_ddlDesignations"]/option[15]').click()
-#designation_choice = driver.find_element(By.ID, 'select2-result-label-314').click()
+  #designation_dropdown = driver.find_element(By.ID, 's2id_PC16725_ddlDesignation').click
+  #designation_dropdown.send_keys('High Impact Research')
+  #designation_dropdown.select_by_visible_text('High Impact Research')
+  #designation_choice = driver.find_element(By.XPATH, '//*[@id="PC16725_ddlDesignations"]/option[15]').click()
+  #designation_choice = driver.find_element(By.ID, 'select2-result-label-314').click()
 
-gift_type = driver.find_element(By.XPATH, '//*[@id="PC16725_rdoGiftType"]/tbody/tr[3]/td/label').click()
+  gift_type = driver.find_element(By.XPATH, '//*[@id="PC16725_rdoGiftType"]/tbody/tr[3]/td/label').click()
 
-driver.implicitly_wait(5) #wait for page to update
+  driver.implicitly_wait(5) #wait for page to update
 
-instalments = driver.find_element(By.NAME, 'PC16725$txtInstallments')
-instalments.send_keys('3')
+  instalments = driver.find_element(By.NAME, 'PC16725$txtInstallments')
+  instalments.send_keys('3')
 
-title = Select(driver.find_element(By.ID, 'PC16725_DonationCapture1_cboTitle'))
-title.select_by_visible_text('Dr')
+  title = Select(driver.find_element(By.ID, 'PC16725_DonationCapture1_cboTitle'))
+  title.select_by_visible_text('Dr')
 
-fname = driver.find_element(By.ID, 'PC16725_DonationCapture1_txtFirstName')
-fname.send_keys('Jane')
+  fname = driver.find_element(By.ID, 'PC16725_DonationCapture1_txtFirstName')
+  fname.send_keys('Jane')
 
-lname = driver.find_element(By.ID, 'PC16725_DonationCapture1_txtLastName')
-lname.send_keys('Doe')
+  lname = driver.find_element(By.ID, 'PC16725_DonationCapture1_txtLastName')
+  lname.send_keys('Doe')
 
-# country = Select(driver.find_element(By.XPATH, '//*[@id="PC16725_DonationCapture1_AddressCtl_dd_Country"]'))
-# country.select_by_visible_text('Belgium')
-# driver.implicitly_wait(5) #wait for page to update
+  # country = Select(driver.find_element(By.XPATH, '//*[@id="PC16725_DonationCapture1_AddressCtl_dd_Country"]'))
+  # country.select_by_visible_text('Belgium')
+  # driver.implicitly_wait(5) #wait for page to update
 
-address = driver.find_element(By.XPATH, '//*[@id="PC16725_DonationCapture1_AddressCtl_tb_AddressLine"]')
-address.send_keys('123 Lalaland Street')
+  address = driver.find_element(By.XPATH, '//*[@id="PC16725_DonationCapture1_AddressCtl_tb_AddressLine"]')
+  address.send_keys('123 Lalaland Street')
 
-suburb = driver.find_element(By.XPATH, '//*[@id="PC16725_DonationCapture1_AddressCtl_tb_CityAUS"]')
-suburb.send_keys('Wonderland')
+  suburb = driver.find_element(By.XPATH, '//*[@id="PC16725_DonationCapture1_AddressCtl_tb_CityAUS"]')
+  suburb.send_keys('Wonderland')
 
-state = Select(driver.find_element(By.XPATH, '//*[@id="PC16725_DonationCapture1_AddressCtl_dd_StateAUS"]'))
-state.select_by_visible_text('VIC')
+  state = Select(driver.find_element(By.XPATH, '//*[@id="PC16725_DonationCapture1_AddressCtl_dd_StateAUS"]'))
+  state.select_by_visible_text('VIC')
 
-postcode = driver.find_element(By.XPATH, '//*[@id="PC16725_DonationCapture1_AddressCtl_tb_ZipAUS"]')
-postcode.send_keys('1234')
+  postcode = driver.find_element(By.XPATH, '//*[@id="PC16725_DonationCapture1_AddressCtl_tb_ZipAUS"]')
+  postcode.send_keys('1234')
 
-phone = driver.find_element(By.XPATH, '//*[@id="PC16725_DonationCapture1_txtPhone"]')
-phone.send_keys('123 456 789')
+  phone = driver.find_element(By.XPATH, '//*[@id="PC16725_DonationCapture1_txtPhone"]')
+  phone.send_keys('123 456 789')
 
 
-email = driver.find_element(By.XPATH, '//*[@id="PC16725_DonationCapture1_txtEmail"]')
-email.send_keys('this@email.com')
+  email = driver.find_element(By.XPATH, '//*[@id="PC16725_DonationCapture1_txtEmail"]')
+  email.send_keys('this@email.com')
 
-#payment_method = driver.find_element(By.XPATH, '//*[@id="PC16725_DonationCapture1_rdoPaymentOption_1"]').click()
-#driver.implicitly_wait(5) #wait for page to update
+  #payment_method = driver.find_element(By.XPATH, '//*[@id="PC16725_DonationCapture1_rdoPaymentOption_1"]').click()
+  #driver.implicitly_wait(5) #wait for page to update
 
-card_name = driver.find_element(By.NAME, 'PC16725$DonationCapture1$txtCardholder')
-card_name.send_keys('Jane Doe')
+  card_name = driver.find_element(By.NAME, 'PC16725$DonationCapture1$txtCardholder')
+  card_name.send_keys('Jane Doe')
 
-card_number = driver.find_element(By.NAME, 'PC16725$DonationCapture1$txtCardNumber')
-card_number.send_keys('0000000000')
+  card_number = driver.find_element(By.NAME, 'PC16725$DonationCapture1$txtCardNumber')
+  card_number.send_keys('0000000000')
 
-card_type = Select(driver.find_element(By.NAME, 'PC16725$DonationCapture1$cboCardType'))
-card_type.select_by_visible_text('Visa')
+  card_type = Select(driver.find_element(By.NAME, 'PC16725$DonationCapture1$cboCardType'))
+  card_type.select_by_visible_text('Visa')
 
 #to submit and/or close the window
 #driver.implicitly_wait(20) #wait for page to update
 
 #donate_btn = driver.find_element(By.NAME, 'PC16725$btnNext').click()
 
-#browser.quit()
+finally:
+  driver.quit()
 
